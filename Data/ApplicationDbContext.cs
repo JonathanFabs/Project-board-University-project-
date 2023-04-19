bad7fbbfca3f51d668b67d7a417e8888fcd28f35
JonathanFabs <82349180+JonathanFabs@users.noreply.github.com>
2023-04-19 17:28:04 +0100

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using JonathanCw2.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JonathanCw2.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Company>Companies { get; set; }
+        public DbSet<User>Users { get; set; }
+        public DbSet<Ticket>Tickets { get; set; }
+        public DbSet<Project>Projects { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
